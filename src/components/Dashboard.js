bba088e7b0bd7e35060e2f2754c1d4eb5bbadb3c
krish-1712 <krishkannan1712@gmail.com>
2023-07-08 22:55:31 +0530

    hike

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -9,23 +9,30 @@ const Dashboard = ({ children }) => {
         <h4>
           <p className="you"> <i className="fab fa-youtube"></i>YouTube</p><br></br>
           <i className="fa-solid fa-house"></i>
+
           <Link to="/home" id="dash">Home</Link><br></br>
         </h4>
         <hr className="hr"></hr>
-        <div className="mute">
-          <i className="fa-solid fa-film"></i>   <Link to="/movies" id="his">Movies</Link><br></br>
-          <i className="fas fa-newspaper"></i>
-          <Link to="/news" id="like">News</Link><br></br>
-          <hr></hr>
-        </div>
+
+        <i className="fa-solid fa-film"></i>
+
+        <Link to="/movies" id="his">Movies</Link><br></br>
+        <div style={{ "paddingTop": "23px" }}><i className="fas fa-newspaper"></i>
+          <Link to="/news" id="like">News</Link><br></br></div>
+
+        <hr></hr>
+
         <i className="fas fa-fire"></i>
+
         <Link to="/trend" id="trend">Trending</Link><br></br>
         <hr ></hr>
         <i className="fas fa-flag"></i>
 
+
         <Link to="/help" id="help">Help</Link><br></br>
-        <i className="far fa-comment"></i>
-        <Link to="/feedback" id="send">Send Feedback</Link><br></br>
+        <div style={{ "paddingTop": "20px" }}>
+          <i className="far fa-comment"></i>
+          <Link to="/feedback" id="send">Send Feedback</Link><br></br></div>
         <hr></hr>
         <p id="about">About Press Copyright<br></br>
           Contact us Creators<br></br>
