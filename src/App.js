b74f3f49e718f3dfea4456c4e6e18da261a693fe
krish-1712 <krishkannan1712@gmail.com>
2023-09-15 23:37:11 +0530

    log

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import News from './components/News';
 import Movies from './components/Movies';
 import Forgot from './components/Forgot';
 import Password from './components/Password';
-// export const url = "http://localhost:8000"
+
 export const url = "https://media-application-backend.onrender.com"
 
 
